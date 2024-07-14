524b00d89567f0ff02588f6d814f65592bedaf93
Anna <annahoege@gmx.de>
2024-07-14 19:25:37 +0200

    intersection observer, image component and layout adjustments

---
+++ src/scripts/intersectionObserver.js
@@ -0,0 +1,26 @@
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.8
+}
+
+const io = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            if (entry.target.classList.contains('left')) {
+                entry.target.classList.add('fade-in-left')
+            } else if (entry.target.classList.contains('right')) {
+                entry.target.classList.add('fade-in-right')
+            } else {
+                entry.target.classList.add('fade-in-forward')
+            }
+        }
+    });
+}, options)
+
+const elements = document.querySelectorAll('.animated');
+
+elements.forEach((el) => {
+    io.observe(el);
+})
+
